515339e77a205f1f02a69f466ad758cb89ab4b2e
Akancha0322 <151225526+Akancha0322@users.noreply.github.com>
2024-03-11 18:09:51 +0530

    Add files via upload

---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ListingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: String,
+    url: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews : [
+    {
+    type: Schema.Types.ObjectId,
+    ref : "Review",
+    }
+  ]
+});
+
+const Listing = mongoose.model("Listing", ListingSchema);
+module.exports = Listing;
